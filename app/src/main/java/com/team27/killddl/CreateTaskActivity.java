2c8868dead27601544cae97d66602aa62d99fd3c
Alexander Kyle Chua <kyle.chua16@gmail.com>
2018-10-17 11:18:00 -0700

    Fixed but where selecting a color crashed the app. Created event listener for Save button on Create Task screen

--- app/src/main/java/com/team27/killddl/CreateTaskActivity.java
+++ app/src/main/java/com/team27/killddl/CreateTaskActivity.java
@@ -3,6 +3,8 @@ package com.team27.killddl;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateTaskActivity extends AppCompatActivity {
 
@@ -16,6 +18,19 @@ public class CreateTaskActivity extends AppCompatActivity {
         toolbar = getSupportActionBar();
         toolbar.setElevation(0);
         toolbar.setTitle("Create a New Task");
+
+
+        Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                // Debugging
+                // view.offsetTopAndBottom(-50);
+
+                // Implement validation and Task creation here
+            }
+        });
+
     }
 
 }
